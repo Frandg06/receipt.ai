730746bb95b4316af708499bcd83517f362bead1
frandg06 <fdiez86@gmail.com>
2025-05-04 03:04:35 +0200

    🔨 refactor(api): actualizar lógica de subida de imágenes y mejorar manejo de errores

--- src/services/get-tmp-image.ts
+++ src/services/get-tmp-image.ts
@@ -1,23 +1,23 @@
 export const getTmpImageUrl = async (file: File) => {
   const formData = new FormData();
-  formData.append('image', file);
-  const respose = await fetch(
-    `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}&expiration=600`,
-    {
-      method: 'POST',
-      body: formData,
-    }
-  );
+  formData.append('file', file);
+  const respose = await fetch(`https://fran-cloud.fdiez86.workers.dev/api/v1/upload`, {
+    headers: {
+      Authorization: `f8e2a97b4c3d6e5f1a0b9c8d7e6f5a4b3c2d1e0f9a8b7c6d5e4f3a2b1c0d9e8f7`,
+    },
+    method: 'POST',
+    body: formData,
+  });
 
   if (!respose.ok) {
     throw new Error('Se ha producido un error al subir la imagen');
   }
 
-  const { data } = await respose.json();
+  const data = await respose.json();
 
-  if (!data || !data.url) {
+  if (!data || !data?.image?.url) {
     throw new Error('Se ha producido un error al subir la imagen');
   }
 
-  return data.url;
+  return data.image.url;
 };
