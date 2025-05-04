9771fddcae0afb26e7f7647afd953369a4bf6e70
frandg06 <fdiez86@gmail.com>
2025-05-04 03:10:43 +0200

    🔨 refactor(api): reemplazar clave de autorización estática por variable de entorno

--- src/services/get-tmp-image.ts
+++ src/services/get-tmp-image.ts
@@ -3,7 +3,7 @@ export const getTmpImageUrl = async (file: File) => {
   formData.append('file', file);
   const respose = await fetch(`https://fran-cloud.fdiez86.workers.dev/api/v1/upload`, {
     headers: {
-      Authorization: `f8e2a97b4c3d6e5f1a0b9c8d7e6f5a4b3c2d1e0f9a8b7c6d5e4f3a2b1c0d9e8f7`,
+      Authorization: import.meta.env.VITE_IMAGE_API_KEY,
     },
     method: 'POST',
     body: formData,
